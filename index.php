b0202577d630defad5b726ebe3cde63538bea137
vaytorh@gmail.com <vaytorh@gmail.com>
2018-07-15 14:21:21 -0300

    Require do menu em todas as páginas, alterado os caminhos para a url

--- index.php
+++ index.php
@@ -7,31 +7,10 @@
       header("Location: pages/home.php");
     }
     
+    //Carregando a barra de navegação
+    require_once("pages/nav.php");
+
 ?>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>App Help Desk</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <style>
-      .card-login {
-        padding: 30px 0 0 0;
-        width: 350px;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
-
-  <body>
-
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
-        <img src="./img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        App Help Desk
-      </a>
-    </nav>
 
     <div class="container">    
       <div class="row">
