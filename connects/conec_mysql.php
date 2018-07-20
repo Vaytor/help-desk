04b7fcd403b86e2090296abb8fa9c10a022a417a
Vaytor <38585125+Vaytor@users.noreply.github.com>
2018-07-20 14:16:14 -0300

    Refactoring da conexão

--- connects/conec_mysql.php
+++ connects/conec_mysql.php
@@ -13,15 +13,15 @@ class db{
     public function conec_mysql(){
         
         //Conexão sendo criada
-        $con = mysqli_connect($this->server, $this->user, $this->senha, $this->database);
+        $mysqli = mysqli_connect($this->server, $this->user, $this->senha, $this->database);
 
         //Verificando se ocorreu erro ao criar a conexão
-        if(!$con){
-            die("Erro ao se conectar ao banco de dados".mysqli_error());
+        if(!$mysqli){
+            die("Erro: ".$mysqli->error());
         }
 
         //Retornando a conexão
-        return $con;
+        return $mysqli;
 
     }
 
